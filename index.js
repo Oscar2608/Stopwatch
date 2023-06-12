8b3f673236ab0926afa20fb23700f77c3bb53b89
Oscar2608 <127762712+Oscar2608@users.noreply.github.com>
2023-06-11 22:28:29 -0400

    Update index.js

--- index.js
+++ index.js
@@ -16,14 +16,14 @@ function startTimer() {
     }, 10);
     
     startButtonEl.disabled = true;
-    startButtonEl.disabled = false;
+    stopButtonEl.disabled = false;
 }
 
 function formatTime(elapsedTime){
     const miliseconds = Math.floor((elapsedTime % 1000) / 10);
     const seconds = Math.floor((elapsedTime % (1000 * 60)) / 1000);
     const minutes = Math.floor((elapsedTime % (1000 * 60 * 60)) / (1000 * 60));
-    const hours = Math.floor((elapsedTime / (1000 * 60 * 60));
+    const hours = Math.floor(elapsedTime / (1000 * 60 * 60));
     return (
       (hours ? (hours > 9 ? hours : "0" + hours) : "00")
     + ":" +                        
@@ -31,7 +31,7 @@ function formatTime(elapsedTime){
     + ":" +
       (seconds ? (seconds > 9 ? seconds : "0" + seconds) : "00")
     + "." +
-      (miliseconds > 9 ? ; milliseconds : "0" + milliseconds)
+      (miliseconds > 9 ? miliseconds : "0" + miliseconds)
     ); 
 }
 
@@ -51,6 +51,6 @@ function resetTimer(){
     stopButtonEl.disabled = true;
 }
 startButtonEl.addEventListener("click", startTimer);
-stopButtonEl.addEventListener("click", startTimer);
-resetButtonEl.addEventListener("click", startTimer);
+stopButtonEl.addEventListener("click", stoptTimer);
+resettButtonEl.addEventListener("click", resetTimer);
 
